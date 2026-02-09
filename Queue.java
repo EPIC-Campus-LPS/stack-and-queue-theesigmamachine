3ecae6f7f08b968f7fe76e2bd77c92f8aed59bdd
Bora <boraayhanwebsite@gmail.com>
2026-02-09 12:58:33 -0700

    Implemented queue methods and test case. Queue complete, needs documentation.

--- Queue.java
+++ Queue.java
@@ -1,2 +1,45 @@
-public class Queue {
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+public static class Queue {
+    private final ArrayList<Integer> q;
+
+    public Queue(ArrayList<Integer> inp) {
+        q = inp;
+    }
+
+    public void add(int el) {
+        q.add(el);
+    }
+
+    public int remove() {
+        if(q.isEmpty()) throw new NoSuchElementException();
+        return (q.removeLast());
+    }
+
+    public int peek() {
+        if(q.isEmpty()) throw new NoSuchElementException();
+        return (q.getLast());
+    }
+
+    public int size() {
+        return q.size();
+    }
+
+    public boolean isEmpty() {
+        return (q.isEmpty());
+    }
+}
+
+public static void main(String[] args) {
+    ArrayList<Integer> s = new ArrayList<Integer>(0);
+    Queue q = new Queue(s);
+    q.add(3);
+    q.add(10);
+    System.out.println(q.remove());
+    System.out.println(q.peek());
+    q.add(32);
+    q.add(-20);
+    System.out.println(q.size());
+    System.out.println(q.isEmpty());
 }
