7c6a12f3689e55ff3cea42b71d23fbc00b835c85
cadenbents <cbtech4581@gmail.com>
2026-02-09 13:31:41 -0700

    Stack Finished

--- Stack.java
+++ Stack.java
@@ -1,3 +1,60 @@
-public class Stack {
-// Penis
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+public static class Stack {
+    private ArrayList<Integer> numArr;
+
+    public Stack(ArrayList<Integer> s){
+        numArr = s;
+    }
+
+    public void push(int element){
+        numArr.add(element);
+    }
+
+    public int pop(){
+        if (numArr.isEmpty()) throw new NoSuchElementException();
+        else{
+            return numArr.removeLast();
+        }
+
+    }
+
+    public int peek(){
+        if (numArr.isEmpty()) throw new NoSuchElementException();
+        else{
+            return numArr.getLast();
+        }
+    }
+
+    public int size(){
+        return numArr.size();
+    }
+
+
+    public boolean isEmpty(){
+        return (numArr.isEmpty());
+    }
+
+
+
+
+}
+
+public static void main(String[] args) {
+    ArrayList<Integer> arr = new ArrayList<Integer>(0);
+    Stack s = new Stack(arr);
+    s.push(7);
+    s.push(15);
+    s.push(35);
+    s.push(2);
+    System.out.println(s.peek());
+    s.push(22);
+    System.out.println(s.pop());
+    s.peek();
+    s.push(14);
+    s.pop();
+    s.pop();
+    System.out.println(s.size());
+    System.out.println(s.pop());
 }
